4223d4821d15007b5d301b971069781d036ea5ac
Soham Shah <sohamshxh@gmail.com>
2025-07-16 09:47:42 +0530

    Create Program3.cs

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 9;
+            int num2 = 0;
+            int temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("Swapped Numbers are : " + num1);
+            Console.WriteLine("Swapped Numbers are : " + num2);
+
+        }
+    }
+}
