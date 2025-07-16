edf9219b7186f94069cab3d872b1482fbb22e996
Soham Shah <sohamshxh@gmail.com>
2025-07-16 10:12:22 +0530

    Create Program2.cs

---
+++ Lab Exercise/Lab Exercise - 2/Program2.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2 = 3;
+            int result=num1+num2;
+            Console.WriteLine("Swapped Numbers are : " + result);
+
+        }
+    }
+}
