a23c559622856970e146cfe93de6f623bc6f0d8b
Soham Shah <sohamshxh@gmail.com>
2025-07-16 10:13:36 +0530

    Create Program3.cs

---
+++ Lab Exercise/Lab Exercise - 2/Program3.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Inputs
+            Console.Write("Enter your number : ");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num > 0)
+            {
+                Console.Write("number is postive");
+            }
+            else
+            {
+                Console.Write("number is negative");
+            }
+
+        }
+    }
+}
