326d7d8acc3fa0ea9eabf507bc7bc215f5c60dee
Soham Shah <sohamshxh@gmail.com>
2025-07-16 10:27:36 +0530

    Create Program3.cs

---
+++ Lab Exercise/Lab Exercise - 1/Program3.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2+1);
+        }
+    }
+}
