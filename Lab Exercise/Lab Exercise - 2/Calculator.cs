b8a5cf5a929cc1aaf3b56adca39e945282d97d84
Soham Shah <sohamshxh@gmail.com>
2025-07-17 08:02:42 +0530

    Add files via upload

---
+++ Lab Exercise/Lab Exercise - 2/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperations
+{
+    public class Calculator
+    {
+        int num1 = 6,num2=4;
+        public int add(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
