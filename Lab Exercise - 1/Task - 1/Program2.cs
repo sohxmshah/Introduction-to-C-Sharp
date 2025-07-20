26f75e4cb90e1d4155a188920eb2f187cffe4e02
Soham Shah <sohamshxh@gmail.com>
2025-07-20 17:09:27 +0530

    Create Program2.cs

---
+++ Lab Exercise - 1/Task - 1/Program2.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Developers!");
+            Console.WriteLine("I want to be Full Stack Developer!");
+            Console.WriteLine(3 + 3);
+
+            // Declaration of variables
+            int number1 = 10;
+            Console.WriteLine(number1);
+
+            double myDouble = 5.99;
+            Console.WriteLine(myDouble);
+
+            char myChar = 'S';
+            Console.WriteLine(myChar);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+            string myString = "Hello Coders!";
+            Console.WriteLine(myString);
+        }
+    }
+}
