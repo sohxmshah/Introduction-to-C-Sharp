303fc3ad32aa4b753419d26259c18163802acf6d
Soham Shah <sohamshxh@gmail.com>
2025-07-21 11:12:13 +0530

    Create Program3.cs

---
+++ Lab Exercise - 2/Task - 2/Program3.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote.");
+            }
+
+        }
+    }
+}
