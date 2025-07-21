571af15ae2137b9b3be23a0b10a89a4a0ad88a1d
Soham Shah <sohamshxh@gmail.com>
2025-07-21 11:04:23 +0530

    Create Program.cs

---
+++ Lab Exercise - 2/Task - 2/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("number1");
+            Console.Write("myDouble");
+            Console.WriteLine("myChar");
+            Console.WriteLine("myBool");
+            Console.WriteLine("myString");
+            Console.Write("myObject");
+        }
+    }
+}
