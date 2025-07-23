ae3c4e3ed386621a772e2a87045db3281e820ef8
Soham Shah <sohamshxh@gmail.com>
2025-07-23 16:16:26 +0530

    Create Program14.cs

---
+++ Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Program14.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 21;
+            float myFLoat = 21.9f;
+            char myChar = 'S';
+            double myDouble = 21.99;
+            string myStr = "John Watkin";
+
+            Console.WriteLine(myNum);
+            Console.WriteLine(myFLoat);
+            Console.WriteLine(myChar);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myStr);
+        }
+
+    }
+}
