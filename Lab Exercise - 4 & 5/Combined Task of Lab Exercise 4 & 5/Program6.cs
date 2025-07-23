1e9d8d7f2504878c17c1bb93dea1e0d44afad6ec
Soham Shah <sohamshxh@gmail.com>
2025-07-23 15:08:43 +0530

    Create Program6.cs

---
+++ Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Program6.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i= 1; i<= num; i++)
+            {
+                if(i % 2!= 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
