d3e8343d3fd5b47a7d77c2fe026a481aebe74f46
Soham Shah <sohamshxh@gmail.com>
2025-07-23 16:01:21 +0530

    Create Program10.cs

---
+++ Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Do-While Loop/Program10.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }while (i <= 10); 
+        }
+    }
+}
