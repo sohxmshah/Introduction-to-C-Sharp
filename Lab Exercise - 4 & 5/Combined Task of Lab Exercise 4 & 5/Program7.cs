ac882c023e1c1e701624bd1276374cba6ed8e365
Soham Shah <sohamshxh@gmail.com>
2025-07-23 15:13:47 +0530

    Create Program7.cs

---
+++ Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Program7.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i= 1; i<= 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum of 1 to 10 is : " + sum);
+        }
+    }
+}
