8ddda9e4ebe7911a94449c34561cd135bbee4952
Soham Shah <sohamshxh@gmail.com>
2025-07-23 15:17:33 +0530

    Update Program7.cs

--- Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Program7.cs
+++ Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Program7.cs
@@ -10,12 +10,18 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            int sum = 0;
+            int sum = 0,product=1;
             for (int i= 1; i<= 10; i++)
             {
                 sum += i;
+                product *= i;
+
             }
+            // Sum of 1 to 10 numbers
             Console.WriteLine("Sum of 1 to 10 is : " + sum);
+
+            // Product of 1 to 10 numbers
+            Console.WriteLine("Product of 1 to 10 is : " + product);
         }
     }
 }
