1052173452a3496e26936393690260de3b69536c
Soham Shah <sohamshxh@gmail.com>
2025-07-23 14:19:51 +0530

    Create Program2.cs

---
+++ Lab Exercise - 3/Task - 3/Program2.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i<= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
