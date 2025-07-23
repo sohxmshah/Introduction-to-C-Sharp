43d0d6edb2bc94b25c5ee6828cb5f462128109ec
Soham Shah <sohamshxh@gmail.com>
2025-07-23 20:37:54 +0530

    Create Program25.cs

---
+++ Lab Exercise - 4 & 5/Combined Task of Lab Exercise 4 & 5/Program25.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("array elements :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }   
+        }
+    }
+}
